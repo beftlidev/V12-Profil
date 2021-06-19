aae1230c9ada0de932014866395b6f43f8a00461
iUgur <77486662+iUgur@users.noreply.github.com>
2021-06-19 07:47:04 +0300

    Update profil.js

--- Komutlar/profil.js
+++ Komutlar/profil.js
@@ -5,83 +5,43 @@ exports.run = async (client, message, args) => {
 let kullanıcı = message.mentions.users.first() 
 
 let yas = db.fetch(`yas_${message.author.id}`)
-let yas2 = db.fetch(`yas_${kullanıcı.author.id}`) 
 let aciklama = db.fetch(`aciklama_${message.author.id}`) 
-let aciklama2 = db.fetch(`aciklama_${kullanıcı.author.id}`)
 let durum = db.fetch(`durum_${message.author.id}`) 
-let durum2 = db.fetch(`durum_${kullanıcı.author.id}`) 
 let durume = db.fetch(`durume_${message.author.id}`) 
-let durume2 = db.fetch(`durume_${kullanıcı.author.id}`) 
 let banner = db.fetch(`banner_${message.author.id}`) 
-let banner2 = db.fetch(`banner_${kullanıcı.author.id}`) 
 let xbox = db.fetch(`xbox_${message.author.id}`) 
-let xbox2 = db.fetch(`xbox_${kullanıcı.author.id}`) 
 let youtube = db.fetch(`youtube_${message.author.id}`) 
-let youtube2 = db.fetch(`youtube_${kullanıcı.author.id}`) 
 let github = db.fetch(`github_${message.author.id}`) 
-let github2 = db.fetch(`github_${kullanıcı.author.id}`) 
 
-let xbox = new MessageButton() 
+let xboxb = new MessageButton() 
 .setLabel(`${xbox || "?"}`)
 .setID("xboxprofil")
 .setEmoji("855659097311805460")
 .setStyle("green")
 
-let xbox2 = new MessageButton() 
-.setLabel(`${xbox2 || "?"}`)
-.setID("xbox2profil")
-.setEmoji("855659097311805460")
-.setStyle("green") 
-
-let youtube = new MessageButton() 
+let youtubeb = new MessageButton() 
 .setLabel(`YouTube`)
 .setURL(`${youtube || "https://youtube.com"}`)
 .setStyle("url")
 
-let youtube = new MessageButton() 
-.setLabel(`YouTube`)
-.setURL(`${youtube2 || "https://youtube.com"}`)
-.setStyle("url")
-
-let youtube = new MessageButton() 
+let githubb = new MessageButton() 
 .setLabel(`Github`)
 .setURL(`${github || "https://github.com"}`)
 .setStyle("url")
 
-let youtube = new MessageButton() 
-.setLabel(`Github`)
-.setURL(`${github2 || "https://github.com"}`)
-.setStyle("url")
-
-if (kullanıcı) {
-const embed = new Discord.MessageEmbed() 
-.setTitle("Profil") 
-.addField(`İsim:`, `${kullanıcı.author.username}`) 
-.addField(`Yaş:`, `${yas2 || "Ayarlanmamış"}`) 
-.addField(`Açıklama:`, `${aciklama2 || "Ben çok güzel bir kelebeğim"}`) 
-.addField(`Durum:`, `${durume2 || ""} ${durum2 || "Ben çok güzel bir çiçeğim"}`) 
-.setImage(`${banner2 || ""}`) 
-.setColor("GREEN") 
-message.channel.send({
-embed: embed, 
-buttons: [xbox, youtube, github]
-}) 
-} 
-
-else {
 const embed1 = new Discord.MessageEmbed() 
 .setTitle("Profil") 
-.addField(`İsim:`, message.author.username) 
-.addField(`Yaş:`, `${yas || "Ayarlanmamış"}`) 
-.addField(`Açıklama:`, `${aciklama || "Ben çok güzel bir kelebeğim"}`) 
-.addField(`Durum:`, `${durume || ""} ${durum || "Ben çok güzel bir çiçeğim"}`)
+.addField(`İsim`, message.author.username) 
+.addField(`Yaş`, `${yas || "Ayarlanmamış"}`) 
+.addField(`Açıklama`, `${aciklama || "Ben çok güzel bir kelebeğim"}`) 
+.addField(`Durum`, `${durume || ""} ${durum || "Ben çok güzel bir çiçeğim"}`)
 .setImage(`${banner || ""}`)
 .setColor("GREEN") 
+.setThumbnail(message.author.avatarURL({ dynamic: true, size: 2048 })) 
 message.channel.send({
-embed: embed, 
-buttons: [xbox2, youtube2, github2]
+embed: embed1, 
+buttons: [xboxb, youtubeb, githubb]
 }) 
-} 
 
 } 
 exports.conf = {
