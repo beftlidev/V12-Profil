ad9794cd4a8cc9531f335dc73667eb8daef5d58c
iUgur <77486662+iUgur@users.noreply.github.com>
2021-06-22 17:15:09 +0300

    Update coin-sil.js

--- Komutlar/coin-sil.js
+++ Komutlar/coin-sil.js
@@ -5,8 +5,8 @@ exports.run = async (client, message, args) => {
 
 if(message.author.id !== "753842258457002036") if(message.author.id !== "753842258457002036") return message.channel.send("<:carpi:855750448711467058> Bu komut <@753842258457002036>' a özeldir!!")
 
-db.add(`coin_${args[0]`, -args[1])
-message.inlineReply(`<:tik:804253394326913085> <@${args$[0]>n Adlı kişiden ${args[1]} coin sildim!`) 
+db.subtract(`coin_${args[0]}`, args[1])
+message.inlineReply(`<:tik:804253394326913085> <@${args[0]}> Adlı kişiden ${args[1]} coin sildim!`) 
 
 } 
 exports.conf = {
